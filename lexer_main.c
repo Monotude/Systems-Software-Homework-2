d52fe995fb4dae9d3b76d41e7ad0bfaae5ae71c6
Tyler Knoop <143290179+Monotude@users.noreply.github.com>
2023-10-15 17:23:13 -0400

    Main completed

--- lexer_main.c
+++ lexer_main.c
@@ -4,7 +4,19 @@ Made by: Tyler Knoop, Brady Napier, and Shaun Gorllapati
 10/20/2023
 */
 
-int main(int argv, char *argvs[])
+#include "lexer.h"
+#include "utilities.h"
+
+int main(int argc, char *argv[])
 {
-    
+    if (argc != 2)
+    {
+        bail_with_error("One file name needed.");
+    }
+
+    else
+    {
+        lexer_init(argv[1]);
+        lexer_output();
+    }
 }
